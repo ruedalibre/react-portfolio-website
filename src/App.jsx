20eaef50940556c0728926e55db32522045081bf
ruedalibre <andresruedalibre@gmail.com>
2022-03-21 18:10:13 -0500

    geneal clasess styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Header from './components/header/Header.jsx'
+import Nav from './components/nav/Nav.jsx'
+import About from './components/about/About.jsx'
+import Experience from './components/experience/Experience.jsx'
+import Services from './components/services/Services.jsx'
+import Portfolio from './components/portfolio/Portfolio.jsx'
+import Testimonials from './components/testimonials/Testimonials.jsx'
+import Contact from './components/contact/Contact.jsx'
+import Footer from './components/footer/Footer.jsx'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header/>
+      <Nav/>
+      <About/>
+      <Experience/>
+      <Services/>
+      <Portfolio/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
+      
+    </>
   )
 }
 
