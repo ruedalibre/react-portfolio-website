50cff6803ac9142a321686b1f4e2eec7cacfe9b7
ruedalibre <andresruedalibre@gmail.com>
2022-03-21 13:33:26 -0500

    Initial config

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import App from './App';
+import './index.css';
+
+/* Aquí conecto el archivo index con el root del proyecto */
+ReactDOM.render(<App/>, document.querySelector("#root"))
+
