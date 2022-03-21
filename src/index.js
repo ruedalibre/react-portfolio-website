3c06e93e48132ee9c585e5deb21b70fd6715d8c6
ruedalibre <andresruedalibre@gmail.com>
2022-03-21 15:58:46 -0500

    creation of all components folders

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import ReactDOM from "react-dom";
-import App from './App';
-import './index.css';
+import ReactDOM from "react-dom"
+import App from './App'
+import './index.css'
 
 /* Aquí conecto el archivo index con el root del proyecto */
 ReactDOM.render(<App/>, document.querySelector("#root"))
