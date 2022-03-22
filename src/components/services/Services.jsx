90b2769990cc960f831e6b03be7d60cd6641371e
ruedalibre <andresruedalibre@gmail.com>
2022-03-22 14:50:21 -0500

    services component

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,94 @@
 import React from 'react'
 import './services.css'
+import {BiCheck} from 'react-icons/bi'
 
 const Services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF WEB DEVELPMENT */}
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Data Science</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF DATA SCIENCE*/}
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Research and Education</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF RESEARCH AND EDUCATION */}
+
+      </div>
+    </section>
   )
 }
 
