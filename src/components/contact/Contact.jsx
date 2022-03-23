370866418e809f732f2bb97971233ae548956225
ruedalibre <andresruedalibre@gmail.com>
2022-03-23 00:46:31 -0500

    contact me component part I

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
 import './contact.css'
+import {MdEmail} from 'react-icons/md'
+import {BsLinkedin} from 'react-icons/bs'
+import {IoLogoWhatsapp} from 'react-icons/io'
+
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>andresruedalibre@gmail.com</h5>
+            <a href="mailto:andresruedalibre@gmail.com" target='_blank'>Send Email</a>
+          </article>
+          <article className="contact__option">
+            <BsLinkedin className='contact__option-icon'/>
+            <h4>LinkedIn</h4>
+            <h5>andres-perez-develop</h5>
+            <a href="https://www.linkedin.com/in/andres-perez-develop/" target='_blank'>Add Me To Your Network</a>
+          </article>
+          <article className="contact__option">
+            <IoLogoWhatsapp className='contact__option-icon'/>
+            <h4>Whatsapp</h4>
+            <h5>(+57) 324 3585922</h5>
+            <a href="https://api.whatsapp.com/send?phone=+573243585922" target='_blank'>Send a message</a>
+          </article>
+
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required/>
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
