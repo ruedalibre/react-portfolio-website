6182c873dd8d015447cc435e075f3ae5172da97a
ruedalibre <andresruedalibre@gmail.com>
2022-03-24 19:21:12 -0500

    deploy part I

---
+++ src/firebase/firebaseConfig.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB5pt4FO_nDwGuBMD86yxm5iaZTR_wjyAc",
+  authDomain: "react-portfolio-website-30bda.firebaseapp.com",
+  projectId: "react-portfolio-website-30bda",
+  storageBucket: "react-portfolio-website-30bda.appspot.com",
+  messagingSenderId: "276805070587",
+  appId: "1:276805070587:web:df6d6d005a90aa336c0a5e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
