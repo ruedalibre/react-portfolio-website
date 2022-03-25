20dcef01aabc6f2a88e48e684ef39d6533d90365
ruedalibre <andresruedalibre@gmail.com>
2022-03-24 21:27:10 -0500

    deploy part II

--- src/firebase/firebaseConfig.jsx
+++ src/firebase/firebaseConfig.jsx
@@ -1,19 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB5pt4FO_nDwGuBMD86yxm5iaZTR_wjyAc",
-  authDomain: "react-portfolio-website-30bda.firebaseapp.com",
-  projectId: "react-portfolio-website-30bda",
-  storageBucket: "react-portfolio-website-30bda.appspot.com",
-  messagingSenderId: "276805070587",
-  appId: "1:276805070587:web:df6d6d005a90aa336c0a5e"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+// Creo una variable para acceder fácilmente a la base de datos en cualquiera de los demás archivos
+const db = getFirestore();
+
+export default db;
